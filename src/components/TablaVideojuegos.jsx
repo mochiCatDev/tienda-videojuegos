6dbd982198d5d0a682b62fe11234d8b217c580e2
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-09 13:40:12 -0500

    fix: optimizar scroll horizontal y evitar ruptura de texto en celdas para celulares, media querys

--- src/components/TablaVideojuegos.jsx
+++ src/components/TablaVideojuegos.jsx
@@ -1,19 +1,24 @@
-import './TablaVideojuegos.css';
+import "./TablaVideojuegos.css";
 
 export const TablaVideojuegos = ({ juegos }) => {
   return (
     <div className="tabla-container">
       <table>
         <thead>
-          <tr><th>Título</th><th>Género</th><th>Plataforma</th><th>Progreso</th></tr>
+          <tr>
+            <th>Título</th>
+            <th>Género</th>
+            <th>Plataforma</th>
+            <th>Progreso</th>
+          </tr>
         </thead>
         <tbody>
           {juegos.map((j) => (
             <tr key={j.id}>
-              <td data-label="Título">{j.titulo}</td>
-              <td data-label="Género">{j.genero}</td>
-              <td data-label="Plataforma">{j.plataforma}</td>
-              <td data-label="Progreso">
+              <td>{j.titulo}</td>
+              <td>{j.genero}</td>
+              <td>{j.plataforma}</td>
+              <td>
                 <div className="progress-bar">
                   <div
                     className="progress-fill"
