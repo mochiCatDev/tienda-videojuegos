fd41f919e114d55714ecfd303a7a8178d082d186
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-10 11:32:23 -0500

    fix: dejar por defecto el archivo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-//import { defineConfig } from 'vite'
-//import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-//export default defineConfig({
-//  plugins: [react()],
-//})
-
-
-// Eliminar estas lineas al finalizar los cambios
-import { defineConfig } from 'vite'
-
 export default defineConfig({
-  server: {
-    allowedHosts: [
-      'agent-quickstep-elixir.ngrok-free.dev'
-    ]
-  }
+  plugins: [react()],
 })
